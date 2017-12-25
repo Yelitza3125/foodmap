49eb90562e1c465ac7ab64a901713fee32372a0c
Yelitza3125 <deyel_99@hotmail.com>
2017-12-25 11:37:02 -0500

    Comentando el código

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-//   redireccionando a otro htmt con tiempo
 
+/*Funcion para desaparecer el gif y aparecer el titulo. */
 setInterval(function title(){
     $('.logo').replaceWith('<h1 class=title><strong>Itadakimasu </strong></h1>')
     $('.title').animate({fontSize: '4em'}, 'swing');
-    
-
 }, 3000);
 
 /*
