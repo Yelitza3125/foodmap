963e374fc4c6b07c5fb5b72c6d7303dac396d509
Yelitza3125 <deyel_99@hotmail.com>
2017-12-26 02:58:50 -0500

    Añadiendo la estructura carrusel

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,48 @@
 $(document).ready(function() {
+    /*
     $('.info-title').animate({
-        padding: '0.5em 1em' }, 'slow');
+        padding: '0.5em 1em' }, 'slow'); */
+
+        var target = 0;
         
+        function loadPage() {
+            
+          var $buttons = $('.control');
+          var $previous = $('.previous');
+          var $next = $('.next');
         
+          $buttons.click(changeImage);
+          $previous.click(previousImage);
+          $next.click(nextImage);
+        };
+        
+        function changeImage() {
+          target = parseInt($(this).data('target'));
+          showImage(target);
+        };
+        
+        function previousImage(e) {
+          e.preventDefault();
+          target = target - 1;
+          target = (target < 0) ? 4 : target;
+          showImage(target);
+          $buttons.css('background', 'blue');
+        };
+        
+        function nextImage(e) {
+          e.preventDefault();
+          target = target + 1;
+          target = (target > 4) ? 0 : target;
+          showImage(target);
+        };
+        
+        function showImage(target) {
+          var $lastSlide = $('div.active');
+          var $slide = $("div[data-slide='" + target + "']");
+          $lastSlide.removeClass('active');
+          $slide.addClass('active');
+        };
+     
+
 });    
 
