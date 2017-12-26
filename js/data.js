ba3bab6ee372376379d0d6b4e5fe346db6115e36
Yelitza3125 <deyel_99@hotmail.com>
2017-12-26 16:45:50 -0500

    Creando el archivo js con la data

---
+++ js/data.js
@@ -0,0 +1,56 @@
+var foodMap = {
+    'Edo Sushi Bar': {
+        'Ubicación': {
+            'San Borja':['Av. San Borja Sur 663'], 
+            'Miraflores': ['Calle Berlín 601'], 
+            'Magdalena':['Calle José Cossio 292'], 
+            'San Miguel': ['Av. Monseñor Jorge Dintilhac 665'], 
+            'La Molina': ['Jr. Las Caobas 180'], 
+            'Surco' :['CC. Jockey Plaza CP4-9'], 
+            'San Isidro':['Av. Jorge Basadre 275'] 
+        },
+        'Página web': 'https://edosushibar.com/platos-calientes/agemonos/',
+        'Califiación': 4,
+        'Platos': ['Sashimi', 'Nigiri', 'Gunkan', 'Makis']
+    },
+
+    'Hanzo': {
+        'Ubicación': {
+            'Surco': ['Av. Primavera 1494, Santiago de Surco'], 
+            'San Isidro':['Av. Conquistadores 598, San Isidro - Lima.']
+        }, 
+        'Página web': 'http://hanzo.com.pe/',
+        'Califiación': 4,
+        'Platos': ['Sashimi', 'Nigiri', 'Gunkan', 'Makis', 'Ramen']
+    },
+
+    'Tanuki': {
+        'Ubicación': {
+            'Miraflores': ['Calle 2 de Mayo 215, Miraflores, Lima']
+        },
+        'Página web': 'https://www.facebook.com/pg/TanukiRestaurantjapones/about/?ref=page_internal',
+        'Califiación': 5,
+        'Platos': ['Sashimi', 'Nigiri', 'Gunkan', 'Makis', 'Ramen', 'Bebidas Japonesas', 'Rolls']
+    },
+
+    'Tokio Ramen': {
+        'Ubicación': {
+            'Jesús María': ['Calle Tizón y Bueno 663']
+        },
+        'Página web': 'https://www.facebook.com/jctsramen/?ref=br_rs',
+        'Califiación': 3,
+        'Platos': ['Ramen']
+    },
+
+    'Ko Asian Kitchen': {
+        'Ubicación': {
+            'Miraflores':['Av. Malecón de la Reserva 610, Local 207,(Centro Comercial Larcomar)'],
+            'Jesús María': [' Av. Salaverry cdra. 23. Plaza Gourmet, nivel 0, (Centro Comercial Real Plaza ']
+        },
+        'Página web': 'http://koasiankitchen.com/',
+        'Califiación': 5,
+        'Platos': ['Sashimi', 'Nigiri', 'Gunkan', 'Makis', 'Ramen', 'Sushi', 'Rolls']
+    },
+
+
+}
